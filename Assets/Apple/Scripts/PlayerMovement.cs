fdfda81da1fc8076b681ea138f641ee5e9f8d1a3
Crona Xu <cx640@nyu.edu>
2022-09-19 16:13:22 -0400

    Folders created, camera changed

---
+++ Assets/Apple/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float yaw = 0.0f, pitch = 0.0f;
+    private Rigidbody rb;
+
+    [SerializeField] float walkSpeed = 5.0f, sensitivity = 2.0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(rb.transform.position, Vector3.down, 1 + 0.001f))
+            rb.velocity = new Vector3(rb.velocity.x, 5.0f, rb.velocity.z);
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    void Look()
+    {
+        pitch -= Input.GetAxisRaw("Mouse Y") * sensitivity;
+        pitch = Mathf.Clamp(pitch, -90.0f, 90.0f);
+        yaw += Input.GetAxisRaw("Mouse X") * sensitivity;
+        Camera.main.transform.localRotation = Quaternion.Euler(pitch,yaw,0);
+    }
+
+    void Movement()
+    {
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * walkSpeed;
+        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0.0f, Camera.main.transform.right.x);
+        Vector3 wishDirection= (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.velocity.y);
+        rb.velocity = wishDirection;
+    }
+}
