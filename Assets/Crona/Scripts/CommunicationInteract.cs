bae026f81f9d4b6903041976c56370f196628d03
Alexis Jin <xj606@nyu.edu>
2022-10-17 01:48:03 -0400

    CommunicationSystem_CameraFix
    Fixed camera issues;
    SetActive(false) Eric's screen when on CommunicationSystem camera. SetActive(true) after exit.

--- Assets/Crona/Scripts/CommunicationInteract.cs
+++ Assets/Crona/Scripts/CommunicationInteract.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CommunicationInteract : MonoBehaviour
 {
     public GameObject UseECanvas, ExitCanvas;
     public Transform ComputerUsingPosition;
     public Camera ComputerCamera;
+    public GameObject EricScreen;
     private int IsInPos = 0;
     private Camera MainCam;
     private bool IsInside;
 
-    public GameObject IslandCode;
-    public GameObject CommunicationCode;
+    public TMP_InputField IslandCode;
+    public TMP_InputField CommunicationCode;
     public GameObject sendButton;
     public GameObject Crosshair;
 
@@ -42,23 +44,30 @@ public class CommunicationInteract : MonoBehaviour
 
         if (Input.GetKey(KeyCode.E))
         {
-
+            CommunicationCode.interactable = true;
+            IslandCode.interactable = true;
             if (IsInPos == 0)
             {
-                
+                Debug.Log("InputField activated");
                 ComputerCamera.transform.position = Camera.main.transform.position;
                 ComputerCamera.transform.rotation = Camera.main.transform.rotation;
                 MainCam = Camera.main;
                 Camera.main.enabled = false;
+                EricScreen.SetActive(false);
                 ComputerCamera.enabled = true;
                 StartCoroutine(LerpingPlayerToComputerPos(ComputerUsingPosition.position, ComputerUsingPosition.rotation, 1f, ComputerCamera.transform));
+                IslandCode.ActivateInputField();
             }
         }
         if (Input.GetKey(KeyCode.Escape))
         {
             if (IsInPos == 2)
             {
+                CommunicationCode.interactable = false;
+                IslandCode.interactable = false;
+                Debug.Log("InputField deactivated");
                 StartCoroutine(LerpingPlayerBackToMain(MainCam.transform.position, MainCam.transform.rotation, 1f, ComputerCamera.transform));
+                IslandCode.DeactivateInputField();
             }
         }
     }
@@ -82,10 +91,6 @@ public class CommunicationInteract : MonoBehaviour
         Transformee.rotation = targetRotation;
         ExitCanvas.SetActive(true);
         IsInPos = 2;
-        //IslandCode.SetActive(true);
-        //CommunicationCode.SetActive(true);
-        //sendButton.SetActive(true);
-        
 
     }
 
@@ -93,9 +98,6 @@ public class CommunicationInteract : MonoBehaviour
 
     IEnumerator LerpingPlayerBackToMain(Vector3 targetPosition, Quaternion targetRotation, float duration, Transform Transformee)
     {
-        //IslandCode.SetActive(false);
-        //CommunicationCode.SetActive(false);
-        //sendButton.SetActive(false);
         ExitCanvas.SetActive(false);
         IsInPos = 1;
         float time = 0;
@@ -115,6 +117,7 @@ public class CommunicationInteract : MonoBehaviour
         UseECanvas.SetActive(true);
         Crosshair.SetActive(true);
         MainCam.enabled = true;
+        EricScreen.SetActive(true);
         ComputerCamera.enabled = false;
     }
 }
