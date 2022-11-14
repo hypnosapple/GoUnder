239d6f4be5111b5f8c7eb4eeb7dac1fdfa658634
Crona Xu <cx640@nyu.edu>
2022-11-14 12:14:14 -0500

    subtitle to main scene

--- Assets/Crona/Scripts/Subtitle/SubtitleManager.cs
+++ Assets/Crona/Scripts/Subtitle/SubtitleManager.cs
@@ -29,7 +29,7 @@ public class SubtitleManager : MonoBehaviour
         if (i < amount)
         {
             subtitleText.text = "";
-            subtitleText.DOText(currentContent[i], 1f);
+            subtitleText.DOText(currentContent[i], 2f);
             Debug.Log(currentContent[i]);
             yield return new WaitForSeconds(visibleTimeList[i]);
             StartCoroutine(Display(i + 1));
