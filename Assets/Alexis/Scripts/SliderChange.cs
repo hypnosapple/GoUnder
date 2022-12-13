1cd81ce396b0e1becbd3d036593d61bb418dd5e5
Alexis Jin <xj606@nyu.edu>
2022-12-13 15:29:04 -0500

    test

--- Assets/Alexis/Scripts/SliderChange.cs
+++ Assets/Alexis/Scripts/SliderChange.cs
@@ -8,15 +8,12 @@ public class SliderChange : MonoBehaviour
     public Slider slider;
     float fillTime = 0f;
     public CheckInput checkInput;
-    public AudioSource loadingSound;
-
     public void Update()
     {
         slider.value = Mathf.Lerp(slider.minValue, slider.maxValue, fillTime);
         fillTime += 0.375f * Time.deltaTime;
         if (slider.value == slider.maxValue)
         {
-            loadingSound.enabled = false;
             checkInput.ShowNoResponse();
         }
     }
