c7cf4f0c6bbdbd6016649fd54f916edc144d1722
CronaXu <cx640@nyu.edu>
2023-05-13 03:38:57 -0400

    build settings

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/2/2023 7:52:54 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "2023/5/13 3:38:45";}}
